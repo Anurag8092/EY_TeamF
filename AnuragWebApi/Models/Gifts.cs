e2e3414cf5abeddad332c209d4e7505898edbf7f
Anurag Das <66762616+Anurag8092@users.noreply.github.com>
2022-04-12 22:00:20 +0530

    Add files via upload

---
+++ AnuragWebApi/Models/Gifts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Gift_Auth.Models
+{
+    public partial class Gifts
+    {
+        public int GiftId { get; set; }
+        public string GiftName { get; set; }
+        public long Price { get; set; }
+        public int Quantity { get; set; }
+        public string GiftImg { get; set; }
+    }
+}
---
+++ AnuragWebApi/Models/GiftsRepo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gift_Auth.Models
+{
+    public class GiftsRepo : IGiftsRepo
+    {
+        private GiftTool_ProjDBContext _giftctx;
+        //private byte[] key;
+
+        public GiftsRepo()
+        {
+            _giftctx = new GiftTool_ProjDBContext();
+        }
+
+        public GiftsRepo(GiftTool_ProjDBContext giftctx)
+        {
+            _giftctx = giftctx;
+        }
+        public async Task<List<Gifts>> GetAll()
+        {
+            try
+            {
+                return await _giftctx.Gifts.ToListAsync<Gifts>();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
